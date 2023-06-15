9205c2b1cae39cc7e3a28c8fb082ec87d00f4312
emanuele.buchicchio <emanuele.buchicchio@smartpeg.eu>
2023-06-15 15:39:41 +0200

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ReclutamentoSondaggio.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    private const string CodesFilePath = "codes.txt";
+    private const string linkPrefix = "https://example.com?code=";
+
+    private static readonly object fileLock = new object();
+    private List<string> AvailableCodes { get; set; }
+
+    public string Link { get; private set; }
+
+    public void OnGet()
+    {
+        PopAccessCodeFromListWithLock();
+    }
+
+    private void PopAccessCodeFromListWithLock()
+    {
+        //log the thread id
+        _logger.LogInformation($"PopAccessCodeFromListWithLock. Thread id: {Thread.CurrentThread.ManagedThreadId}");
+        lock (fileLock)
+        {
+            AvailableCodes = ReadCodesFromFile();
+            string code = AvailableCodes[0];            
+            AvailableCodes.RemoveAt(0);
+            UpdateCodesFile();
+            Link = GenerateLinkWithCode(code);            
+        }
+        
+    }
+
+    private List<string> ReadCodesFromFile()
+    {
+        var codes = new List<string>();
+
+        using (var reader = new StreamReader(CodesFilePath))
+        {
+            
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                codes.Add(line.Trim());
+            }
+        }
+
+        return codes;
+    }
+
+    private string GenerateLinkWithCode(string code)
+    {                
+        return  $"{linkPrefix}{code}";
+        
+    }
+
+    private void UpdateCodesFile()
+    {
+        using (var writer = new StreamWriter(CodesFilePath))
+        {
+            foreach (var code in AvailableCodes)
+            {
+                writer.WriteLine(code);
+            }
+        }
+    }    
+}
